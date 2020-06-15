426f5428df2cf3b4fdcd191f0861990b82527772
Yusup Edilbiev <chohban@bk.ru>
2020-06-15 11:04:22 +0300

    style improvments

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -7,7 +7,7 @@ export class StoreService {
   content = [
     {id:1, text: 'Выиграть олимпиаду в Токио', done: false, category:'Цели', important: true},
     {id:2, text: 'Купить новую приору 2018 года', done: false, category:'Покупки', important: false},
-    {id:3, text: 'Купить фотоаппарат до свадьбы', done: true, category:'Покупки', important: false},
+    {id:3, text: 'Купить молоко', done: true, category:'Покупки', important: false},
     {id:4, text: 'Встретиться с Мариной', done: false, category:'Личные дела', important: false},
     {id:5, text: 'Прочитать 52 книги до конца года', done: false, category:'Цели', important: false},
   ];
